4352f07c2352a62ca09893d51731dac281480d46
tuncayalt <tuncayalt@gmail.com>
2014-03-30 13:41:53 +0300

    tool.java modified

--- src/tool/tool.java
+++ src/tool/tool.java
@@ -2,7 +2,7 @@ package tool;
 
 public class tool {
 	
-	//this feature is extra. This variable shows the stack size of the composite iterator.
+	//this feature is extra. This variable shows the stack size of the composite iterator...
 	//Used in order to display the parts as a tree structure in the console.
 	public static int stackSize;
 }
